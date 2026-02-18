20afffe96a88c5ace644c8a4a3e7300eaf843b62
nayon-kumar <nayon.cse55@gmail.com>
2026-02-18 12:53:12 +0600

    Welcome section designed

--- script/login.js
+++ script/login.js
@@ -1,22 +1,24 @@
 document.getElementById("loginBtn").addEventListener("click", function () {
-  //   Get the mobile number
   const phoneNumber = document.getElementById("phoneNumber").value;
-  console.log(phoneNumber);
-  // Get the pin
   const pin = document.getElementById("pin").value;
-  console.log(pin);
-  // Match mobile number and pin
+
+  const alertHeading = document.getElementById("alertHeading");
+  const alertText = document.getElementById("alertText");
+
+  alertHeading.classList.remove("text-error", "text-success");
+
   if (phoneNumber == "01234567890" && pin == "1234") {
-    // True >> Home page
-    document.getElementById("alertHeading").innerText = "Success!";
-    document.getElementById("alertText").innerText = "Login successfull!";
+    alertHeading.classList.add("text-success");
+    alertHeading.innerText = "Success!";
+    alertText.innerText = "Login successful!";
+
     document.getElementById("success").addEventListener("click", function () {
-      window.location.assign("home.html");
+      window.location.href = "home.html";
     });
   } else {
-    // False >> Alert
-    document.getElementById("alertHeading").innerText = "Error!";
-    document.getElementById("alertText").innerText =
-      "Invalid phone number or pin! Use phone number: 01234567890 and pin: 1234 for test.";
+    alertHeading.classList.add("text-error");
+    alertHeading.innerText = "Error!";
+    alertText.innerText =
+      "Invalid phone number or pin! Use mobile number: 01234567890 and pin: 1234 for test.";
   }
 });
