930a51479316d3777ae53b3fe260f4805641a662
nayon-kumar <nayon.cse55@gmail.com>
2026-02-18 08:31:45 +0600

    Login functionality added

--- script/login.js
+++ script/login.js
@@ -1,1 +1,17 @@
-
+document.getElementById("loginBtn").addEventListener("click", function () {
+  //   Get the mobile number
+  const phoneNumber = document.getElementById("phoneNumber").value;
+  console.log(phoneNumber);
+  // Get the pin
+  const pin = document.getElementById("pin").value;
+  console.log(pin);
+  // Match mobile number and pin
+  if (phoneNumber == "01234567890" && pin == "1234") {
+    // True >> Home page
+    alert("Login successfull!");
+    window.location.assign("/home.html");
+  } else {
+    // False >> Alert
+    alert("Invallid phone number or pin!");
+  }
+});
